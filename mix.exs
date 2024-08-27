3d3d2c3eb757f42c996f0324a8d991af5ae7baa0
red-jade <katagorikal@gmail.com>
2024-08-27 13:51:40 +0700

    Upgrade to Elixir 1.17.2 / OTP 27

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Exa.Json.MixProject do
 
   @lib  :exa_json
   @name "Exa Json"
-  @ver  "0.2.1"
+  @ver  "0.2.2"
 
   # umbrella project
   @exa {:exa,
@@ -31,7 +31,7 @@ defmodule Exa.Json.MixProject do
       app: @lib,
       name: @name,
       version: @ver,
-      elixir: "~> 1.15",
+      elixir: "~> 1.17",
       erlc_options: [:verbose, :report_errors, :report_warnings, :export_all],
       start_permanent: Mix.env() == :prod,
       deps: [@exa|exa_deps] ++ local_deps(),
